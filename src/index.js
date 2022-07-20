e1746c5cd3ebe9b7c98777bb76495a0d19a6cd9c
Zoe Mendez <zoemendez316@gmail.com>
2022-07-20 14:17:06 -0600

    Done with assignment, but the test isn't working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildTaskList(e.target['new-task-description'].value)
+    form.reset()
+  })
+})
+
+
+function buildTaskList(task){
+  const list = document.getElementById('tasks')
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${task}   `
+  list.appendChild(li)
+  li.appendChild(btn)
+  console.log(list)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+
+
+
+
+
+
